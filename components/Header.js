a65de22376a56dada8ef57dfa2d759c8fb8300ef
ofuji <soutoumasunaga@gmail.com>
2022-05-12 16:21:57 +0900

    リンクモジュールでヘッダーを作成した

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+
+import Link from 'next/link'
+import HeaderStyles from './Header.module.css'
+
+export function Header(){
+    return(
+      <header className={HeaderStyles.header}>
+        <Link href="/">
+            <a className={HeaderStyles.anchor}>Index</a>
+        </Link>
+        <Link href="/about">
+            <a className={HeaderStyles.anchor}>About</a>
+        </Link>
+        
+      </header>
+    )
+}
