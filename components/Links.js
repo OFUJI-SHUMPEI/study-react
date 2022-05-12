f178cd927983e35543228f07d2ed32cf88193cc0
ofuji <soutoumasunaga@gmail.com>
2022-05-12 16:30:24 +0900

    自分でLinkjsを弄る為のバックアップ

--- components/Links.js
+++ components/Links.js
@@ -1,4 +1,12 @@
 import LinksStyles from './Links.module.css'
+
+const ITEMS = [
+  {href:"https://nextjs.org/docs", title:"Documentation &rarr;", discription:"Find in-depth information about Next.js features and API."},
+  {href:"https://nextjs.org/learn", title:"Learn &rarr;", discription:"Learn about Next.js in an interactive course with quizzes!"},
+  {href:"https://github.com/vercel/next.js/tree/canary/examples", title:"Examples &rarr;", discription:"Discover and deploy boilerplate example Next.js projects."},
+  {href:"https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app", title:"Deploy &rarr;", discription:"Instantly deploy your Next.js site to a public URL with Vercel."}
+]
+
 export function Links(){
     return(
         <div className={LinksStyles.grid}>
