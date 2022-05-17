82690546e37d8ed02ffa52008f4c3acd32d395b3
ofuji <soutoumasunaga@gmail.com>
2022-05-17 11:24:23 +0900

    swrエラー解決

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,21 +1,42 @@
 import { useCallback, useEffect, useReducer } from "react";
 import useSWR from "swr";
 
-export const Posts = () => {
+const fetcher = async (url) => {
+  const responce = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!responce.ok) {
+    throw new Error("エラーが発生したため、データの取得を失敗しました。");
+  }
+
+  const json = responce.json();
+  return json;
+};
+
+const usePosts = () => {
   const { data, error } = useSWR(
     "https://jsonplaceholder.typicode.com/posts",
     fetcher
   );
   console.log({ data, error });
 
-  if (!error && !data) {
+  return {
+    data,
+    error,
+    isLoading: !error && !data,
+    isEmpty: data && data.length === 0,
+  };
+};
+
+export const Posts = () => {
+  const { data, error, isLoading, isEmpty } = usePosts();
+
+  if (isLoading) {
     return <div>ロード中です。</div>;
   }
 
   if (error) {
     return <div>{error.message}</div>;
   }
-  if (data.length === 0) {
+  if (isEmpty) {
     return <div>データは空です。</div>;
   }
 
