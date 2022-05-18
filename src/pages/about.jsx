d425675a9d4ad841418f55441aa05bf1172a7b56
ofuji <soutoumasunaga@gmail.com>
2022-05-19 07:58:18 +0900

    アバウトページの大幅改修

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,36 +1,16 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Footer } from "../components/Footer/index.jsx";
-import { Main } from "../components/Main/index.jsx";
+import Head from "next/head";
 import { Header } from "../components/Header/index.jsx";
 
-const About = ({
-  isShow,
-  handleClick,
-  handleDisplay,
-  text,
-  array,
-  handleChange,
-  handleAdd,
-  doubleCount,
-}) => {
+const About = ({}) => {
   return (
     <div className={styles.container}>
-      <Head></Head>
+      <Head>
+        <title>Index Page</title>
+      </Head>
       <Header />
-      {isShow ? <h1>{doubleCount}</h1> : null}
-      <button onClick={handleClick}>ボタン</button>
-      <button onClick={handleDisplay}>非表示</button>
-      <input type="text" value={text} onChange={handleChange} />
-      <button onClick={handleAdd}>追加</button>
-      <ul>
-        {array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-      <Main page="about" />
-
-      <Footer />
+      <h1>Next.jsで学ぶReact講座</h1>
+      <p>JSONplaceholdeのAPIを色々叩いてみるよ！</p>
     </div>
   );
 };
