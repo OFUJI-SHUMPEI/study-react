4c77f2c50d9aa8971662d9ccad794103c8c968b5
ofuji <soutoumasunaga@gmail.com>
2022-05-21 10:42:56 +0900

    コメントの個別ページをSG化

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,23 +1,40 @@
 import Head from "next/head";
 import { usePost } from "../../../hooks/usePost";
 import Link from "next/link";
+import { SWRConfig } from "swr";
 
-export const getServerSideProps = async (ctx) => {
-  const { id } = ctx.query;
+export const getStaticPaths = async () => {
   const COMMENTS_API = `https://jsonplaceholder.typicode.com/comments`;
   const COMMENTS = await fetch(COMMENTS_API);
-  const COMMENTSData = await user.json();
+  const COMMENTSData = await COMMENTS.json();
+  const paths = COMMENTSData.map((com) => ({
+    params: { id: com.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.params;
+  const COMMENT_API = `https://jsonplaceholder.typicode.com/comment${id}`;
+  const COMMENT = await fetch(COMMENT_API);
+  const COMMENTData = await COMMENT.json();
+  console.log(`${id}のSG化`);
 
   return {
     props: {
       fallback: {
-        [API]: COMMENTSData,
+        [COMMENT_API]: COMMENTData,
       },
     },
   };
 };
 
-const Comment = () => {
+const Comment = (props) => {
+  const { fallback } = props;
   const { comment, userdata, error, isLoading } = usePost();
 
   return (
@@ -26,14 +43,16 @@ const Comment = () => {
       <div>
         <h1>{comment?.name}</h1>
         <p>{comment?.body}</p>
-        {comment?.name ? (
-          <div>
-            Created By{" "}
-            <Link href={`../../users/${comment?.postId}`}>
-              <a>{userdata?.name}</a>
-            </Link>
-          </div>
-        ) : null}
+        <SWRConfig value={{ fallback }}>
+          {comment?.name ? (
+            <div>
+              Created By{" "}
+              <Link href={`../../users/${comment?.postId}`}>
+                <a>{userdata?.name}</a>
+              </Link>
+            </div>
+          ) : null}
+        </SWRConfig>
       </div>
     </div>
   );
